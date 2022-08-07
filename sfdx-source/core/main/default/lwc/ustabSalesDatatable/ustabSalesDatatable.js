60a0e02003f719d085f5310e1bd044c28340283e
yurgeo <yuri.georgiyev@advancedcommunities.com>
2022-08-08 01:27:03 +0300

    Implemented custom datatable for sales users.

---
+++ sfdx-source/core/main/default/lwc/ustabSalesDatatable/ustabSalesDatatable.js
@@ -0,0 +1,19 @@
+/*
+ * Custom lightning datatable for sales stats.
+ *
+ * Created by Yurii Heorhiiev on 08.08.2022.
+ * <savthe217@gmail.com>
+ */
+
+import LightningDatatable from 'lightning/datatable';
+import ownerNameTemplate from './ownerName.html';
+
+export default class UstabSalesDatatable extends LightningDatatable {
+    static customTypes = {
+        ownerName: {
+            template: ownerNameTemplate,
+            standardCellLayout: true,
+            typeAttributes: ['smallPhotoUrl'],
+        }
+    }
+}
